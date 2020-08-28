ad127cca0fb9b9507507a88e1cba804516230011
Amibyte <70034843+Amibyte@users.noreply.github.com>
2020-08-28 15:41:50 +0530

    assignment1, assigment 2, conditionals exercise

---
+++ assignment2/question no3.py
@@ -0,0 +1,13 @@
+import math
+num = float(input("enter a no"))
+ans = math.sqrt(num)
+print("Square root of {} is {}".format(num, ans))
+
+
+
+
+
+
+    
+    
+    
---
+++ assignment2/question no4.py
@@ -0,0 +1,4 @@
+hgt = float(input("Enter height: "))
+bdt = float(input("Enter breadth: "))
+area = 0.5 * hgt * bdt
+print(" area = {}".format(area))
